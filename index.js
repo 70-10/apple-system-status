96ecaaa88217c6cb76274b97ddd9cd01d5bb038c
70_10 <7010.dev@gmail.com>
2016-07-05 21:49:15 +0900

    initial

---
+++ index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+"use strict";
+const driver = require("promise-phantom");
+const phantomjs = require("phantomjs-prebuilt");
+const jsdom = require("jsdom").jsdom;
+const Table = require("cli-table2");
+const co = require("co");
+const Entities = require("html-entities").AllHtmlEntities;
+
+const entities = new Entities();
+
+co(function* () {
+  const phantom = yield driver.create({path: phantomjs.path});
+  const page = yield phantom.createPage();
+
+  yield page.open("https://developer.apple.com/system-status/");
+  const result = yield page.evaluate(getServices);
+  const table = new Table({
+    head: ["name", "status"],
+    colWidths: [40, 15]
+  });
+  for (let e of result) {
+    const service = jsdom(e);
+    table.push([entities.decode(service.querySelector(".matrix_p").innerHTML), service.querySelector("span").className]);
+  }
+
+  yield page.close();
+  console.log(table.toString());
+  yield phantom.exit();
+});
+
+function getServices() {
+  var tds = document.querySelectorAll("#dashboard tr>td");
+  var innerHTMLs = [];
+  for (var i = 0; i < tds.length; i++) {
+    innerHTMLs.push(tds[i].innerHTML);
+  }
+  return innerHTMLs;
+}
