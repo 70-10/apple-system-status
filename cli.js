5f74c22f9365e3947e261dcb2f7c600db5d8ee28
70_10 <7010.dev@gmail.com>
2017-07-12 14:00:45 +0900

    [mod] cli.js

--- cli.js
+++ cli.js
@@ -1,3 +1,19 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
 const appleSystemStatus = require(".");
 
-appleSystemStatus().then(r => console.log(r)).catch(console.error);
+appleSystemStatus()
+  .then(services => {
+    if (services.length <= 0) {
+      console.log(chalk.green.bold("All services are available."));
+      return;
+    }
+    services.forEach(s => {
+      console.log(
+        `${chalk.red.underline.bold(s.serviceName)} - ${s.redirectUrl}`
+      );
+      console.log(s.events);
+    });
+  })
+  .catch(console.error);
